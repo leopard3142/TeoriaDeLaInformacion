e65393ffbbcaa4339033ac217342e3c193c74eda
matienzo01 <matiasgutierrez225@gmail.com>
2021-09-16 19:09:27 -0300

    Probabilidades condicionales implementadas

--- src/main/java/Prueba/prueba.java
+++ src/main/java/Prueba/prueba.java
@@ -4,6 +4,7 @@ import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Scanner;
 
@@ -25,7 +26,7 @@ public class prueba {
         }
     }
 
-    public static void main(String[] args) {
+    public static void longVar(String path){
         final int MAX;
         int lecturas = 0;
         char [] caracteres;
@@ -37,14 +38,13 @@ public class prueba {
         MAX = scanner.nextInt();
         caracteres = new char[MAX];
         try {
-            String contenido = Files.readString(Paths.get("text.txt"));
-            //String contenido = Files.readString(Paths.get("text.txt"));
+            String contenido = Files.readString(Paths.get(path));
             StringReader reader = new StringReader(contenido);
             while(reader.read(caracteres, 0, MAX) != -1){
                 int i = 0;
                 lecturas++;
                 cadena = String.valueOf(caracteres);
-                while(i<palabras.size() && !palabras.get(i).getPalabra().equalsIgnoreCase(cadena)){
+                while(i<palabras.size() && !palabras.get(i).getPalabra().equalsIgnoreCase(cadena)){ //recorre la lista de nodos a ver si coincide alguna ocurrencia
                     i++;
                 }
                 if(i==palabras.size()){
@@ -56,7 +56,50 @@ public class prueba {
             calculaProbabilidades(palabras, lecturas);
             imprimeLista(palabras);
         }catch (IOException e){
-            System.out.println("No se encontro el archivo");
+            System.out.println("No se encontró el archivo");
+        }
+    }
+
+    public static void main(String[] args) {
+        //longVar("text.txt");
+        condVar("text.txt");
+    }
+
+    private static void condVar(String path) {
+        String act, ant;
+        char [] lectura =  new char[2];
+        int cantT = 0;
+        double[][] mat = new double[4][4];
+        HashMap<String, Integer> codigos = new HashMap<>();
+
+        codigos.put("00", 0);
+        codigos.put("01", 1);
+        codigos.put("10", 2);
+        codigos.put("11", 3);
+
+        try {
+            String contenido = Files.readString(Paths.get(path));
+            StringReader reader = new StringReader(contenido);
+            reader.read(lectura, 0, 2);
+            ant = String.valueOf(lectura);
+            cantT++;
+            while(reader.read(lectura, 0, 2) != -1){
+                act = String.valueOf(lectura);
+                cantT++;
+                mat[codigos.get(ant)][codigos.get(act)]++; //aumenta la cantidad de ocurrencias en la fila del anterior, columna del actual
+                ant = act;
+            }
+
+            for(int i = 0; i<4 ; i++)
+                for(int j = 0;  j<4 ; j++)
+                    mat[i][j]/=cantT; //se hace el promedio de ocurrencias
+            for(int i = 0; i<4 ; i++){
+                for(int j = 0;  j<4 ; j++)
+                    System.out.print(mat[i][j] + " ");
+                System.out.println();
+            }
+        }catch (IOException e){
+            System.out.println("No se encontró el archivo");
         }
     }
 }
