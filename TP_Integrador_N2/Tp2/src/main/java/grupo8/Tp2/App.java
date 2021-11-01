ec07fe25c9b2011ff62be9bd297a22f469a639aa
Matias <matiasgutierrez225@gmail.com>
2021-11-01 20:52:47 -0300

    Tp parte 2 creadp

---
+++ TP_Integrador_N2/Tp2/src/main/java/grupo8/Tp2/App.java
@@ -0,0 +1,13 @@
+package grupo8.Tp2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
